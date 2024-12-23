f64b2313c06c8384dced57682c8a0409facd30f1
Sunke Venkateswarlu <sunkevenkateswarlu@Sunkes-MacBook-Air.local>
2024-12-23 15:15:05 +0530

    changes

--- CandidateManagement/urls.py
+++ CandidateManagement/urls.py
@@ -25,7 +25,9 @@ urlpatterns = [
     path('candidates/', include('candidates.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # else:
 #     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

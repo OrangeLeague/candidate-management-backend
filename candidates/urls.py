b458ee8057c875fca67a20c6572b9d71abf12b78
Sunke Venkateswarlu <sunkevenkateswarlu@Sunkes-MacBook-Air.local>
2025-01-16 14:51:04 +0530

    chnaes

--- candidates/urls.py
+++ candidates/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('admin/candidates/<int:pk>/update', views.update_candidate, name='update_candidate'),  # Update a candidate by ID
 
     path('time-slots/bulk-schedule/', views.bulk_schedule_time_slots, name='bulk_schedule_time_slots'),
-    path('get-candidates/<int:candidate_id>/time-slots/', views.get_candidate_time_slots, name='get_candidate_time_slots'),
+    path('get-candidates/<uuid:candidate_id>/time-slots/', views.get_candidate_time_slots, name='get_candidate_time_slots'),
     path('get-candidate-name/<uuid:candidate_id>/', views.get_candidate_name, name='get_candidate_name'),
 ]
 
